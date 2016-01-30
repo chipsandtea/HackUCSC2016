29129c0b39ffa4e770fbf97bd66533bc01c7bf73
Christopher Hsiao <hsiao.christopher@gmail.com>
2016-01-29 17:08:15 -0800

    MindNode chart
    MindNode included for planning

--- OMe2/app/src/main/java/com/example/christopher/ome/Member.java
+++ OMe2/app/src/main/java/com/example/christopher/ome/Member.java
@@ -4,4 +4,7 @@ package com.example.christopher.ome;
  * Created by Christopher on 1/29/16.
  */
 public class Member {
+
+    private int memberID;
+    
 }
--- OMe2/app/src/main/java/com/example/christopher/ome/Payment.java
+++ OMe2/app/src/main/java/com/example/christopher/ome/Payment.java
@@ -4,4 +4,6 @@ package com.example.christopher.ome;
  * Created by Christopher on 1/29/16.
  */
 public class Payment {
+    private int PayerID, ReceiverID;
+
 }
